11fbda7e38591505404c1156ea886717c2366109
Ibrahimhamza1 <69169202+Ibrahimhamza1@users.noreply.github.com>
2025-02-10 09:16:00 +0300

    Update Get Computer Details.ps1

--- Miscellaneous/Domain Controller/Get Computer Details.ps1
+++ Miscellaneous/Domain Controller/Get Computer Details.ps1
@@ -1,1 +1,11 @@
+$domains = (Get-ADTrust -Filter *).Name+(Get-ADDomainController).domain
 
+ForEach ($Domain in $Domains){  
+
+try
+{
+Get-ADComputer -Identity "hrsd-altrex-web"  -Server $domain -Properties Name,DNSHostName,CanonicalName,OperatingSystem,OperatingSystemVersion,IPv4Address,LastLogonDate | Select-Object Name,DNSHostName,@{'n'='Domain';'e'={$_.CanonicalName.split("/")[0]}},OperatingSystem,OperatingSystemVersion,IPv4Address,LastLogonDate 
+}
+catch{
+}
+}

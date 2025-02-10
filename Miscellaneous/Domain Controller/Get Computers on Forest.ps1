e5059db501700d9011634ce6a7cbf4e646bd0732
Ibrahimhamza1 <69169202+Ibrahimhamza1@users.noreply.github.com>
2025-02-10 09:15:12 +0300

    Update Get Computers on Forest.ps1

--- Miscellaneous/Domain Controller/Get Computers on Forest.ps1
+++ Miscellaneous/Domain Controller/Get Computers on Forest.ps1
@@ -1,1 +1,12 @@
+$domains = (Get-ADTrust -Filter *).Name+(Get-ADDomainController).domain
 
+$result= ForEach ($Domain in $Domains){  
+try{
+
+Get-ADComputer -Filter 'OperatingSystem -like "Windows*" -and enabled -eq "true"'  -Server $domain -Properties Name,DNSHostName,CanonicalName,OperatingSystem,OperatingSystemVersion,IPv4Address,LastLogonDate | Select-Object Name,DNSHostName,@{'n'='Domain';'e'={$_.CanonicalName.split("/")[0]}},OperatingSystem,OperatingSystemVersion,IPv4Address,LastLogonDate 
+
+} 
+catch{}
+}
+
+$result |Export-Csv devices.csv 
